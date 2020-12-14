4e955b787f365fd3c276a242612494d90d18371f
Sergey Firsov <Sergey.firsov.m2@yandex.ru>
2020-12-14 03:08:00 +0300

    Подготовить проект к работе

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+//const { errors } = require('celebrate');
+
+
+const cors = require('cors');
+
+
+/*const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { requestLogger, errorLogger } = require('./middlewares/Logger.js');
+const NotFound = require('./errors/notFound.js');*/
+
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/newsexplorerdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use('/main', (req, res) => res.send('Hello, world!'));
+
+app.use((req, res/*, next*/) => {
+  res.send('Запрашиваемый ресурс не найден');
+  //next(new NotFound('Запрашиваемый ресурс не найден'));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Ошибка сервера' : message,
+  });
+  next();
+});
+
+app.listen(PORT);
